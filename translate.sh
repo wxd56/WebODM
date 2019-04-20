48118f5a9c13e7f024ceadfb3f47c10ff570bade
Piero Toffanin <pt@masseranolabs.com>
2019-04-20 15:41:18 -0400

    Added I10m.toml file, en locale

--- translate.sh
+++ translate.sh
@@ -1,3 +1,6 @@
-django-admin makemessages --locale=it --ignore=app/templates/app/admin/* --ignore=app/templates/app/registration/*
-django-admin makemessages --locale=it -d djangojs --extension jsx
+for lang in en it
+do
+	django-admin makemessages --locale=$lang --ignore=app/templates/app/admin/* --ignore=app/templates/app/registration/*
+	django-admin makemessages --locale=$lang -d djangojs --extension jsx
+done	
 django-admin compilemessages
