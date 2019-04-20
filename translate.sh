069b9821ab1fe98ac028bee5f9c9da0b049ae309
Piero Toffanin <pt@masseranolabs.com>
2019-04-20 16:35:35 -0400

    I10n.toml changes, translation script changes

--- translate.sh
+++ translate.sh
@@ -1,6 +1,6 @@
-for lang in en it
+for lang in it
 do
-	django-admin makemessages --locale=$lang --ignore=app/templates/app/admin/* --ignore=app/templates/app/registration/*
-	django-admin makemessages --locale=$lang -d djangojs --extension jsx
+	django-admin makemessages --keep-pot --locale=$lang --ignore=app/templates/app/admin/* --ignore=app/templates/app/registration/*
+	django-admin makemessages --keep-pot --locale=$lang -d djangojs --extension jsx
 done	
 django-admin compilemessages
